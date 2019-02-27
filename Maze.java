f710508dc576081d48076a13296e8e5f96bb5b9a
Jessica Kim <jkim00@stuy.edu>
2019-02-27 11:17:16 -0500

    worked on constructor

--- Maze.java
+++ Maze.java
@@ -1,25 +1,35 @@
 import java.util.*;
 import java.io.*;
+
 public class Maze{
+  private char[][]maze;
+  private boolean animate;//false by default
 
-private char[][]maze;
-private boolean animate;//false by default
+  /*Constructor loads a maze text file, and sets animate to false by default.
 
-/*Constructor loads a maze text file, and sets animate to false by default.
+    1. The file contains a rectangular ascii maze, made with the following 4 characters:
+    '#' - Walls - locations that cannot be moved onto
+    ' ' - Empty Space - locations that can be moved onto
+    'E' - the location of the goal (exactly 1 per file)
+    'S' - the location of the start(exactly 1 per file)
 
-  1. The file contains a rectangular ascii maze, made with the following 4 characters:
-  '#' - Walls - locations that cannot be moved onto
-  ' ' - Empty Space - locations that can be moved onto
-  'E' - the location of the goal (exactly 1 per file)
-  'S' - the location of the start(exactly 1 per file)
+    2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
 
-  2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
+    3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then:
+       throw a FileNotFoundException or IllegalStateException
+  */
+  public Maze(String filename) throws FileNotFoundException{
+    File f = new File(filename);
+    Scanner s = new Scanner(f);
+    while (s.hasNextLine()) {
+      String line = s.nextLine();
+      System.out.println(line);
+    }
+    animate = false;
+  }
 
-  3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then:
-     throw a FileNotFoundException or IllegalStateException
-*/
-public Maze(String filename) throws FileNotFoundException{
-  //COMPLETE CONSTRUCTOR
+  public String toString() {
+
   }
 
   private void wait(int millis){
