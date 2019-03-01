62598e80a9c7a5099cea9388cf1d88e0dc2171af
Jessica Kim <jkim00@stuy.edu>
2019-02-28 22:02:40 -0500

    imported exceptions and file and tests constructor

--- Driver.java
+++ Driver.java
@@ -1,18 +1,19 @@
+import java.util.*;
+import java.io.*;
+
 public class Driver{
     public static void main(String[]args){
-      //String filename = "data1.dat";
-      Maze f = new Maze();//true animates the maze.
-      System.out.println(f);
-      // try{
-      //   Maze f;
-      //   f = new Maze(filename);//true animates the maze.
-      //   System.out.println(f);
-      //
-      //   // f.setAnimate(true);
-      //   // f.solve();
-      //   // System.out.println(f);
-      // }catch(FileNotFoundException e){
-      //   System.out.println("Invalid filename: "+filename);
-      // }
+      String filename = "data1.dat";
+      try{
+        Maze f;
+        f = new Maze(filename);//true animates the maze.
+        System.out.println(f);
+      
+        // f.setAnimate(true);
+        // f.solve();
+        // System.out.println(f);
+      }catch(FileNotFoundException e){
+        System.out.println("Invalid filename: "+filename);
+      }
     }
 }
