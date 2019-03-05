a10733235645a46bd7182914feb794d3679a5edc
Jessica Kim <jkim00@stuy.edu>
2019-03-05 01:30:37 -0500

    tests, maze works for all 3 sample mazes

--- Driver.java
+++ Driver.java
@@ -3,15 +3,18 @@ import java.io.*;
 
 public class Driver{
     public static void main(String[]args){
-      String filename = "data1.dat";
+      String filename = "data3.dat";
       try{
         Maze f;
         f = new Maze(filename);//true animates the maze.
         System.out.println(f);
-      
-        // f.setAnimate(true);
-        // f.solve();
+      	
+        f.setAnimate(true);
+        System.out.println(f.solve());
+        // f.putCursor(5,2);
         // System.out.println(f);
+        // f.blockSpace(5,2);
+        System.out.println(f);
       }catch(FileNotFoundException e){
         System.out.println("Invalid filename: "+filename);
       }
